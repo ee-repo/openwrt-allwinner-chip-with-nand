b418293b8ebc4c79a13b30fa62be76c2b5758b85
Benedikt-Alexander Mokroß <bam@icognize.de>
2019-11-14 17:06:54 +0100

    Moved kernel and dtb in a UBIFS for faster loading

--- target/linux/sunxi/image/cortex-a7-v3s-spinand.mk
+++ target/linux/sunxi/image/cortex-a7-v3s-spinand.mk
@@ -7,7 +7,7 @@ define Device/sun8i-v3s-dolphinpi
   SUPPORTED_DEVICES := dolphinpi
   SUNXI_DTS := sun8i-v3s-dolphinpi
 
-  KERNEL := kernel-bin | lzma | uImage lzma 
+  KERNEL := kernel-bin | lzma | uImage lzma | sunxi-kernelubifs
   IMAGES := ubispinand.img.gz
 
   MKUBIFS_OPTS := -F -m $(CONFIG_SUNXI_SPINAND_PAGESIZE) -e $(shell echo $$(($(CONFIG_SUNXI_SPINAND_BLOCKIZE) - (($(CONFIG_SUNXI_SPINAND_PAGESIZE)/1024)*2))))KiB -c 880 -U
@@ -18,11 +18,10 @@ define Device/sun8i-v3s-dolphinpi
   SUBPAGESIZE := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
   VID_HDR_OFFSET := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
   IMAGE_SIZE := 110m
-  KERNEL_SIZE := 4m
+  # KERNEL_SIZE := 5m
   KERNEL_IN_UBI := 1
   UBOOTENV_IN_UBI := 1
-  UBINIZE_PARTS := dtb=$(DTS_DIR)/$$(SUNXI_DTS).dtb=1
-
+  # UBINIZE_PARTS := dtb=$(DTS_DIR)/$$(SUNXI_DTS).dtb=1
   IMAGE/ubispinand.img.gz := \
       sunxi-spinandboot | \
       pad-to $$(CONFIG_SUN8I_V3S_OFFSET_UBI) | \
